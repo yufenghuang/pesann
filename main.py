b9081257cb5d955e2df636ca26223317fe0b0638
yufenghuang <yufeng.chase.huang@gmail.com>
2018-02-02 14:49:15 -0800

    fix typo

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ parser.add_argument("--runtype", choices=[-2,-1,0,1,2], type=int,\
                                    -4=train energy using precalculated features")
 parser.add_argument("inputData", type=str, help="Geometry file. Must include energies and/or forces when training")
 parser.add_argument("--validationSet", type=str)
-parser.add_argument("--testSec", type=str)
+parser.add_argument("--testSect", type=str)
 
 parser.add_argument("--inputFile", type=str, help="Input file specifying the calculation. \
                                                    Keys will be overwritten by command line arguments")
