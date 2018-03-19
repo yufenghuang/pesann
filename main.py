6638ce279f1e8aea611886118cc346c90042b0ec
Yufeng Huang <yufeng.chase.huang@gmail.com>
2018-03-19 10:05:52 -0700

    added special run 5

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ params={
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--task", choices=[-3,-2,-1,0,1,2,100], type=int,
+parser.add_argument("--task", choices=[-3,-2,-1,0,1,2,100,105], type=int,
                     help="task.  2=get energy and forces, \
                                     1=get energy (default), \
                                     0=MD, \
@@ -190,6 +190,9 @@ elif params["task"] == -3:
     
 elif params["task"] == 100:
     md.specialrun4(params)
+    
+elif params["task"] == 105:
+    md.specialrun5(params)
     
 else:
     print("Unrecognized task: ", params["task"])
