ae5a0039d901d483819969f58c6113016bb669bd
Yufeng Huang <yufeng.chase.huang@gmail.com>
2018-03-28 12:03:21 -0700

    print forces for specialrun8

--- md.py
+++ md.py
@@ -779,7 +779,8 @@ def specialrun8(params):
                       "Ei(RMSE)", EiRMSE, "Fi(RMSE)", FiRMSE, "Fnn.Fdft", crossF)
 
                 for iAtom in range(nAtoms2):
-                    print("Si"+str(iAtom), R20[iAtom, 0], R20[iAtom, 1], R20[iAtom,2])
+                    print("Si"+str(iAtom+1), R20[iAtom, 0], R20[iAtom, 1], R20[iAtom,2], Fp[iAtom, 0], Fp[iAtom, 0], Fp[iAtom, 0],
+                          f2[iAtom,0], f2[iAtom,1], f2[iAtom,2])
 
                 sys.stdout.flush()
 
