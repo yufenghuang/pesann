2232e0eebd03e5023f228dd1512055eb0981b60a
Yufeng Huang <yufeng.chase.huang@gmail.com>
2018-03-30 14:56:32 -0700

    fix getData

--- md.py
+++ md.py
@@ -78,6 +78,46 @@ def getRFVmmt(mmtFile):
     return nAtoms, lattice, R, forces, velocities
 
 
+def getData11(dataFile):
+    line = dataFile.readline()
+    if "Iteration" in line:
+        sptline = line.split()
+        nAtoms = int(sptline[0])
+        iIter = int(re.match("(\d*),", sptline[2])[1])
+
+        dataFile.readline()
+        lattice = np.zeros((3, 3))
+        lattice[0, :] = np.array(dataFile.readline().split(), dtype=float)
+        lattice[1, :] = np.array(dataFile.readline().split(), dtype=float)
+        lattice[2, :] = np.array(dataFile.readline().split(), dtype=float)
+
+        dataFile.readline()
+        R = np.zeros((nAtoms, 3))
+        for i in range(nAtoms):
+            sptline = dataFile.readline().split()
+            R[i, :] = np.array(sptline[1:4])
+
+        dataFile.readline()
+        forces = np.zeros((nAtoms, 3))
+        for i in range(nAtoms):
+            sptline = dataFile.readline().split()
+            forces[i, :] = np.array(sptline[1:4])
+
+        dataFile.readline()
+        velocities = np.zeros((nAtoms, 3))
+        for i in range(nAtoms):
+            sptline = dataFile.readline().split()
+            velocities[i, :] = np.array(sptline[1:4])
+
+        R[R > 1] = R[R > 1] - np.floor(R[R > 1])
+        R[R < 0] = R[R < 0] - np.floor(R[R < 0])
+
+        return nAtoms, iIter, lattice, R, forces, velocities
+
+    else:
+        return getData11(dataFile)
+
+
 def specialrun1(params):
     J = 1/1.602177e-19 # eV
     meter = 1e10 # Angstroms
@@ -1055,7 +1095,7 @@ def specialrun11(params):
     saver = tf.train.Saver(list(set(tf.get_collection("saved_params"))))
     with open(params["inputData"], 'r') as mmtFile:
         # nAtoms, lattice, R, F0, V0 = getRFVmmt(mmtFile)
-        nAtoms, iIter, lattice, R, F0, V0, E0 = pyf.getData(mmtFile)
+        nAtoms, iIter, lattice, R, F0, V0 = getData11(mmtFile)
 
     # print(V0)
     # print("Initial Kinetic Energy: V0", np.sum(0.5 * mSi * V0 ** 2 * constA))
