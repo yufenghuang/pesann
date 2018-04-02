df377f9009b326c079f0731736347a90d565b4f1
yufenghuang <yufeng.chase.huang@gmail.com>
2018-04-01 19:49:55 -0700

    fix bug

--- md.py
+++ md.py
@@ -1261,7 +1261,7 @@ def specialrun13(params):
     tfFors = tf.placeholder(tf.float32, shape=(None, 3))
     tfLearningRate = tf.placeholder(tf.float32)
 
-    tfEs, tfFs = tff.tf_getEF(tfCoord, tfLattice, params)
+    tfEs, tfFs = tff.tf_getEF_repulsion(tfCoord, tfLattice, params)
 
     tfEp = (tfEs - tfEngyB) / tfEngyA
     tfFp = tfFs / tfEngyA
