dd5de061d35af2438330fcfff41b86e4ef33674f
Yufeng Huang <yufeng.chase.huang@gmail.com>
2018-04-06 10:43:54 -0700

    test bug

--- py_func.py
+++ py_func.py
@@ -161,6 +161,9 @@ def trainEngy(params):
                      tfEngy: engyDF * params['engyScalerA'] + params['engyScalerB']}
         Ep2 = sess.run(tfEs, feed_dict=feedDict2)
         Ep2 = (Ep2 - params['engyScalerB'])/params['engyScalerA']
+
+        print(Ep2.shape, Ep.reshape(-1).shape)
+        print(engyDF.shape, engyDF.reshape(-1).shape)
         
         Ermse = np.sqrt(np.mean((Ep2.reshape(-1)-engyDF.reshape(-1))**2))
         Emae = np.mean(np.abs(Ep2.reshape(-1) - engyDF.reshape(-1)))
