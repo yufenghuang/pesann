corrected the ensemble average for <J(t)J(0)>

specialTask.py
@@ -718,7 +718,7 @@ def specialTask08(params):
             _,_,Fln = npf.adjMat2adjList(adjMat, Fln.transpose([1, 0, 2]))
             Rln = -Rln
 
-            Jhalf = np.sum(Rln * np.sum(Fln * V0[:, None, :], axis=2)[:, :, None], axis=1).sum(axis=0)
+            Jhalf = np.sum(Rln * np.sum(Fln * V0[:, None, :], axis=2)[:, :, None], axis=1)
 
             return Jhalf
 
@@ -743,14 +743,13 @@ def specialTask08(params):
             R1, Vpos, V0 = MDstep(R0, Vneg, dt, Fp)
             Rhalf = R0 + m(R0[:, 0] / lattice[0, 0])[:, None] * Vpos * dt
 
-            J0 = np.sum(Ep*V0, axis=0)
+            J0 = Ep*V0
             J1 = getJhalf(R0, m(R0[:, 0] / lattice[0, 0])[:, None] * V0)
             J2 = getJhalf(Rhalf, (1-m(R0[:, 0] / lattice[0, 0])[:, None]) * V0) # What velocity?
-            J = J0+J1+J2
-            Jx = J[0]
+            Jt = J0+J1+J2
 
             if iStep == 0:
-                Jx0 = Jx
+                Jt0 = Jt
 
             # print(Ep.shape, J0.shape, J1.shape, J2.shape, J.shape, Jx.shape)
             # print(Jx0, Jx, Jx0*Jx)
@@ -759,7 +758,7 @@ def specialTask08(params):
 
             if (iStep % int(params["nstep"]) == 0) or \
                     ((iStep % int(params["nstep"]) != 0) & (iStep == params["epoch"] - 1)):
-                printXYZ(iStep, R0, V0, Fp, Ep, Jx0*Jx)
+                printXYZ(iStep, R0, V0, Fp, Ep, np.sum(Jt0*Jt, axis=0)[0])
 
 
 def old_specialTask08(params):