285ef16ec85fe32ae0c75a802a875b6a8e6da3e2
Yufeng Huang <yufeng.chase.huang@gmail.com>
2018-05-10 01:29:12 -0700

    force random velocities

--- specialTask.py
+++ specialTask.py
@@ -969,14 +969,12 @@ def specialTask10(params):
         with open(params["inputData"], 'r') as mmtFile:
             nAtoms, iIter, lattice, R, F0, V0 = pyf.getData11(mmtFile)
             V0 = V0 * 1000 * bohr
-            temperature = float(params["T"])
+            V0 = np.random.randn(*V0.shape) * np.sqrt(np.sum(V0 ** 2) * constA*1000*mole)* meter/s
             R1 = R.dot(lattice.T)
             feedDict = {tfCoord: R, tfLattice: lattice}
             Ep, Fp = sess.run((tfEp, tfFp), feed_dict=feedDict)
             Fp = -Fp
             Vpos = V0 - 0.5*Fp/mSi*dt / constA
-            if temperature != 0:
-                Vpos = np.random.randn(*V0.shape) * np.sqrt(kB * temperature/(mSi/(1000*mole))) * meter/s
 
 
         # MD equilibrium loop
