201d7129620efffa0882ada89b8e80734f66c069
Yufeng Huang <yufeng.chase.huang@gmail.com>
2018-05-11 10:32:09 -0700

    move md functions to md_func, untested

--- md_func.py
+++ md_func.py
@@ -88,9 +88,6 @@ def read_structure(fileName, format):
 
 # Andersen thermostat
 def andersen(params):
-    dt = float(params["dt"])
-    coll_prob = float(params["coll_prob"])
-
     tfEngyA = tf.constant(params['engyScalerA'], dtype=tf.float32)
     tfEngyB = tf.constant(params['engyScalerB'], dtype=tf.float32)
 
@@ -111,8 +108,7 @@ def andersen(params):
         # define the function for Ep and Fp
         def getEF(R0, lattice):
             R = np.linalg.solve(lattice, R0.T).T
-            R[R > 1] = R[R > 1] - np.floor(R[R > 1])
-            R[R < 0] = R[R < 0] - np.floor(R[R < 0])
+            R = R - np.floor(R)
             R0 = R.dot(lattice.T)
 
             feedDict = {tfCoord: R, tfLattice: lattice}
@@ -141,6 +137,8 @@ def andersen(params):
         feedDict = {tfCoord: R, tfLattice: lattice}
         E1, F1 = sess.run((tfEp, -tfFp), feed_dict=feedDict)
 
+        dt = float(params["dt"])
+        coll_prob = float(params["coll_prob"])
         Tend = np.sum(0.5 * mSi * V1 ** 2 * constA) / J /(3/2*nAtoms) / kB
         Tbegin = Tend
 
@@ -166,7 +164,7 @@ def andersen(params):
 
                 Temp = Ekin / (3 / 2 * nAtoms) * eV2J / kB
 
-                if ((iStep + 1) % int(params["nstep"]) == 0):
+                if (iStep % int(params["nstep"]) == 0):
                     print(nAtoms)
                     print("iStep", iStep,
                           "lattice", " ".join([str(x) for x in lattice.reshape(-1)]),
@@ -188,7 +186,7 @@ def andersen(params):
 
             Temp = Ekin/(3/2*nAtoms)*eV2J/kB
 
-            if ((iStep + 1) % int(params["nstep"]) == 0):
+            if (iStep  % int(params["nstep"]) == 0):
                 print(nAtoms)
                 print("iStep", iStep,
                       "lattice", " ".join([str(x) for x in lattice.reshape(-1)]),
@@ -200,3 +198,180 @@ def andersen(params):
                           V0[iAtom, 0], V0[iAtom, 1], V0[iAtom, 2],
                           F0[iAtom, 0], F0[iAtom, 1], F0[iAtom, 2])
                 sys.stdout.flush()
+
+            if ((iStep + 1) % int(params["nstep"] * 10) == 0):
+                with open("md.xyz", 'w') as xyzFile:
+                    xyzFile.write(str(nAtoms) + "\n")
+                    xyzFile.write(
+                        "iStep " + str(iStep) + " lattice " + " ".join([str(x) for x in lattice.reshape(-1)]) +
+                        " Epot " + "{:.12f}".format(Epot) + " Ekin " + "{:.12f}".format(Ekin) +
+                        " Etot " + "{:.12f}".format(Etot) + " Temp " + "{:.12f}".format(Temp) +
+                        "\n")
+                    for iAtom in range(nAtoms):
+                        xyzFile.write(
+                            "Si " + str(R0[iAtom, 0]) + " " + str(R0[iAtom, 1]) + " " + str(R0[iAtom, 2]) +
+                            " " + str(V0[iAtom, 0]) + " " + str(V0[iAtom, 1]) + " " + str(V0[iAtom, 2]) +
+                            " " + str(F0[iAtom, 0]) + " " + str(F0[iAtom, 1]) + " " + str(F0[iAtom, 2]) + "\n")
+
+
+# cleaned up version of NVE
+def NVE(params):
+    # special MD run
+    dt = float(params["dt"])
+
+    # setup Tensorflow
+    tfEngyA = tf.constant(params['engyScalerA'], dtype=tf.float32)
+    tfEngyB = tf.constant(params['engyScalerB'], dtype=tf.float32)
+    tfCoord = tf.placeholder(tf.float32, shape=(None, 3))
+    tfLattice = tf.placeholder(tf.float32, shape=(3, 3))
+    tfEs, tfFs = tff.tf_getEF(tfCoord, tfLattice, params)
+    tfEp = (tfEs - tfEngyB) / tfEngyA
+    tfFp = tfFs / tfEngyA
+
+    saver = tf.train.Saver(list(set(tf.get_collection("saved_params"))))
+
+    with tf.Session() as sess:
+
+        # initialize Tensorflow flow
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess, str(params['logDir']) + "/tf.chpt")
+
+        # define the function for Ep and Fp
+
+        def getEF(R0, lattice):
+            R = np.linalg.solve(lattice, R0.T).T
+            R = R - np.floor(R)
+            R0 = R.dot(lattice.T)
+            feedDict = {tfCoord: R, tfLattice: lattice}
+            Ep, Fp = sess.run((tfEp, -tfFp), feed_dict=feedDict)
+
+            return R0, Ep, Fp
+
+        # initialize the atomic positions and velocities
+        nAtoms, lattice, R, V0 = read_structure(params["inputData"], params["format"])
+        R1, Ep, Fp = getEF(R.dot(lattice.T), lattice)
+        Vpos = V0 - 0.5 * Fp / mSi * dt / constA
+
+        # MD loop
+        for iStep in range(params["epoch"]):
+            R0 = R1
+            Vneg = Vpos
+
+            R0, Ep, Fp = getEF(R0)
+            R1, Vpos, V0 = vverlet(R0, Vneg, dt, Fp)
+
+            Epot, Ekin, Etot = getMDEnergies(Ep, V0)
+            Temp = Ekin / (3 / 2 * nAtoms) * eV2J / kB
+
+            # printing the output
+            if (iStep % int(params["nstep"]) == 0):
+                print(nAtoms)
+                print("iStep", iStep,
+                      "lattice", " ".join([str(x) for x in lattice.reshape(-1)]),
+                      "Epot", "{:.12f}".format(Epot), "Ekin", "{:.12f}".format(Ekin),
+                      "Etot", "{:.12f}".format(Etot), "Temp", "{:.12f}".format(Temp))
+                for iAtom in range(nAtoms):
+                    print("Si", R0[iAtom, 0], R0[iAtom, 1], R0[iAtom, 2],
+                          V0[iAtom, 0], V0[iAtom, 1], V0[iAtom, 2],
+                          Fp[iAtom, 0], Fp[iAtom, 1], Fp[iAtom, 2])
+                sys.stdout.flush()
+
+            if ((iStep+1) % int(params["nstep"]*10) == 0):
+                with open("md.xyz", 'w') as xyzFile:
+                    xyzFile.write(str(nAtoms) + "\n")
+                    xyzFile.write("iStep " + str(iStep) + " lattice " + " ".join([str(x) for x in lattice.reshape(-1)]) +
+                                  " Epot " + "{:.12f}".format(Epot) + " Ekin " + "{:.12f}".format(Ekin) +
+                                  " Etot " + "{:.12f}".format(Etot) + " Temp " + "{:.12f}".format(Temp) +
+                                  "\n")
+                    for iAtom in range(nAtoms):
+                        xyzFile.write("Si " + str(R0[iAtom, 0]) + " " + str(R0[iAtom, 1]) + " " + str(R0[iAtom, 2]) +
+                                      " " + str(V0[iAtom, 0]) + " " + str(V0[iAtom, 1]) + " " + str(V0[iAtom, 2]) +
+                                      " " + str(Fp[iAtom, 0]) + " " + str(Fp[iAtom, 1]) + " " + str(Fp[iAtom, 2]) + "\n")
+
+# calculate heat current J(t) for the calculation of the
+# heat current auto-correlation function (HCACF)
+def hcacf(params):
+    # setup Tensorflow
+    tfEngyA = tf.constant(params['engyScalerA'], dtype=tf.float32)
+    tfEngyB = tf.constant(params['engyScalerB'], dtype=tf.float32)
+    tfCoord = tf.placeholder(tf.float32, shape=(None, 3))
+    tfLattice = tf.placeholder(tf.float32, shape=(3, 3))
+    tfEs, tfFs1, tfFs2 = tff.tf_getEFln(tfCoord, tfLattice, params)
+
+    tfEp = (tfEs - tfEngyB) / tfEngyA
+    tfFp = (tfFs1 + tf.reduce_sum(tfFs2, axis=1)) / tfEngyA
+    tfFpq = tfFs2 / tfEngyA
+
+    saver = tf.train.Saver(list(set(tf.get_collection("saved_params"))))
+
+    with tf.Session() as sess:
+
+        # initialize Tensorflow flow
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess, str(params['logDir']) + "/tf.chpt")
+
+        # define the function for Ep and Fp
+        def getEF(R0, lattice):
+            R = np.linalg.solve(lattice, R0.T).T
+            R[R > 1] = R[R > 1] - np.floor(R[R > 1])
+            R[R < 0] = R[R < 0] - np.floor(R[R < 0])
+
+            feedDict = {tfCoord: R, tfLattice: lattice}
+            Ep, Fp, Fpq = sess.run((tfEp, -tfFp, -tfFpq), feed_dict=feedDict)
+
+            return R.dot(lattice.T), Ep, Fp, Fpq
+
+        def getJ(R0, V0, Fln):
+            R = np.linalg.solve(lattice, R0.T).T
+            R[R > 1] = R[R > 1] - np.floor(R[R > 1])
+            R[R < 0] = R[R < 0] - np.floor(R[R < 0])
+
+            idxNb, Rln, maxNb, nAtoms = npf.np_getNb(R, lattice, float(params['dcut']))
+
+            Vln = np.zeros((nAtoms, maxNb, 3))
+            Vln[idxNb>0] = V0[idxNb[idxNb>0]-1]
+
+            Jpot = np.sum(Rln * np.sum(Fln * Vln, axis=2)[:, :, None], axis=1)
+
+            return Jpot
+
+        # initialize the atomic positions and velocities
+        dt = float(params["dt"])
+        nAtoms, lattice, R, V0 = read_structure(params["inputData"], params["format"])
+        R1, Ep, Fp = getEF(R.dot(lattice.T), lattice)
+        Vpos = V0 - 0.5 * Fp / mSi * dt / constA
+
+        # Thermal conductivity MD loop
+        for iStep in range(params["epoch"]):
+            R0 = R1
+            Vneg = Vpos
+
+            R0, Ep, Fp, Fpq = getEF(R0, lattice)
+            R1, Vpos, V0 = vverlet(R0, Vneg, dt, Fp)
+
+            # printing the output
+            Ek = np.sum(0.5 * mSi * V0 ** 2 * constA, axis=1)
+            J0 = (Ep+Ek[:,None])*V0
+            J1 = getJ(R0, V0, Fpq)
+            Jt = np.sum(J0 + J1, axis=0)
+
+            Epot, Ekin, Etot = getMDEnergies(Ep, V0)
+            Temp = Ekin / (3 / 2 * nAtoms) * eV2J / kB
+
+            print(iStep, "Epot", "{:.12f}".format(Epot), "Ekin", "{:.12f}".format(Ekin),
+                  "Etot", "{:.12f}".format(Etot),
+                  "Jx", "{:.12f}".format(Jt[0]), "Jy", "{:.12f}".format(Jt[1]), "Jz", "{:.12f}".format(Jt[2]))
+
+            if ((iStep+1) % int(params["nstep"]*10) == 0):
+                with open("md.xyz", 'w') as xyzFile:
+                    xyzFile.write(str(nAtoms) + "\n")
+                    xyzFile.write("iStep " + str(iStep) + " lattice " + " ".join([str(x) for x in lattice.reshape(-1)]) +
+                                  " Epot " + "{:.12f}".format(Epot) + " Ekin " + "{:.12f}".format(Ekin) +
+                                  " Etot " + "{:.12f}".format(Etot) + " Temp " + "{:.12f}".format(Temp) +
+                                  " Jx " + "{:.12f}".format(Jt[0]) +
+                                  " Jy " + "{:.12f}".format(Jt[1]) +
+                                  " Jz " + "{:.12f}".format(Jt[2]) + "\n")
+                    for iAtom in range(nAtoms):
+                        xyzFile.write("Si " + str(R0[iAtom, 0]) + " " + str(R0[iAtom, 1]) + " " + str(R0[iAtom, 2]) +
+                                      " " + str(V0[iAtom, 0]) + " " + str(V0[iAtom, 1]) + " " + str(V0[iAtom, 2]) +
+                                      " " + str(Fp[iAtom, 0]) + " " + str(Fp[iAtom, 1]) + " " + str(Fp[iAtom, 2]) + "\n")
